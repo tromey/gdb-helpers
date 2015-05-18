67520979725bb9c9e5b06d8137632fe600fd25a8
Tom Tromey <tom@tromey.com>
2015-05-18 14:01:28 -0600

    more docs

--- gdbhelpers/preattach.py
+++ gdbhelpers/preattach.py
@@ -5,7 +5,12 @@ import os
 import subprocess
 
 class Preattach(gdb.Command):
-    """ blah """
+    """Attach to the next instance of a program.
+Usage:
+    preattach BASENAME
+This runs a SystemTap script that watches for the next time a program
+that has the given basename is invoked.  Then, a SIGSTOP is delivered
+to that process, and gdb attaches to it."""
 
     def __init__(self):
         super(Preattach, self).__init__("preattach", gdb.COMMAND_RUNNING,
