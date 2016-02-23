56d443b6fbee192ad838c56c8170c2c6ebc9aa1c
Tom Tromey <tom@tromey.com>
2016-02-23 13:28:42 -0700

    correctly set environment variables

--- gdbhelpers/editor.py
+++ gdbhelpers/editor.py
@@ -5,8 +5,8 @@ if os.getenv("INSIDE_EMACS"):
     # The blocking behavior of "edit" doesn't seem too useful,
     # especially when running inside Emacs, so this just disables it.
     # See the "ecomm" command though.
-    os.putenv("EDITOR", "emacsclient -n")
-    os.putenv("BLOCKING_EDITOR", "emacsclient")
+    os.environ["EDITOR"] = "emacsclient -n"
+    os.environ["BLOCKING_EDITOR"] = "emacsclient"
     # At some point this stopped working automatically.
     # Maybe because Emacs switched back to --fullname?
     gdb.execute('set pagination off', to_string = True)
